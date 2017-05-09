504bb93050290d82f8d7ca754ba6ee3e3ebe319f
Lucas Lago <lucasbenderlago@gmail.com>
2017-05-09 14:51:38 -0300

    Implement kafka producer

--- producer/index.js
+++ producer/index.js
@@ -1,22 +1,26 @@
 const Kafka = require('kafka-node');
 const kafkaClient = require('../kafka');
 const producer = new Kafka.Producer(kafkaClient);
-const TOPIC = 'arroz';
+const TOPIC = 'juraci';
+
+const messages = process.argv.slice(2);
 
 producer.on('ready', () => {
   console.log('producer ready')
 
-  producer.createTopics([TOPIC],false, (err, data) => {
-    if(err) console.log(err)
-    console.log(`topic ${TOPIC} created synchronously`);
-    console.log(data)
-  });
+  //producer.createTopics([TOPIC], false, (err, data) => {
+  //  if(err) console.log(err)
+  //  console.log(`topic ${TOPIC} created synchronously`);
+  //  console.log(data)
+  //});
 
-  // producer.send([{topic: TOPIC, messages: ['eu', 'sou', 'um', 'arroz']}], (err, data) => {
-  //   if(err) console.log(err);
-  //   console.log('topic sent: ', data);
-  // })
+  producer.send([{topic: TOPIC, messages}], (err, data) => {
+    if(err) console.log(err);
+    else {
+      console.log('topic sent: ', data);
+      process.exit(0);
+    }
+  })
 });
 
 producer.on('error', (error) =>  console.log(error));
-
