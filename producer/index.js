acd3b3a2f64f2a4c96b005b53960ed9aa025c935
Lucas Lago <lucasbenderlago@gmail.com>
2017-05-09 15:14:40 -0300

    Create topic on container startup

--- producer/index.js
+++ producer/index.js
@@ -1,26 +1,20 @@
 const Kafka = require('kafka-node');
 const kafkaClient = require('../kafka');
 const producer = new Kafka.Producer(kafkaClient);
-const TOPIC = 'juraci';
 
+const topic = 'darksouls';
 const messages = process.argv.slice(2);
 
 producer.on('ready', () => {
   console.log('producer ready')
-
-  //producer.createTopics([TOPIC], false, (err, data) => {
-  //  if(err) console.log(err)
-  //  console.log(`topic ${TOPIC} created synchronously`);
-  //  console.log(data)
-  //});
-
-  producer.send([{topic: TOPIC, messages}], (err, data) => {
-    if(err) console.log(err);
-    else {
-      console.log('topic sent: ', data);
-      process.exit(0);
+  producer.send([{topic, messages}], (err, data) => {
+    if(err) {
+      console.log(err);
+      return err;
     }
+    console.log('sent topic: ', data);
+    process.exit(0);
   })
 });
 
-producer.on('error', (error) =>  console.log(error));
+producer.on('error', error =>  console.log(error));
