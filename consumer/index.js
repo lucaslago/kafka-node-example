0043ecd1a2e26aa80cb91c7f9fbd8b7be8eaf9cb
Lucas Lago <lucasbenderlago@gmail.com>
2017-05-10 11:37:26 -0300

    Use blizzard wrapper for consumer example

--- consumer/index.js
+++ consumer/index.js
@@ -1,7 +1,29 @@
-const Kafka = require('kafka-node');
-const kafkaClient = require('../kafka');
+const Kafka = require('node-rdkafka');
 
-const consumer = new Kafka.Consumer(kafkaClient, [{ topic: 'darksouls', partition: 0 }]);
+const consumer = new Kafka.KafkaConsumer({ 'group.id': 'kafka', 'metadata.broker.list': 'localhost:9092' });
+const TOPIC_NAME = 'darksouls';
 
-consumer.on('message', message => console.log(message));
-consumer.on('error', error => console.log(error));
+consumer.on('event.log', log => console.log(log))
+
+consumer.on('error', (err) => {
+  console.error('Error from consumer');
+  console.error(err);
+});
+
+consumer.on('ready', (arg) => {
+  console.log('consumer ready.', JSON.stringify(arg));
+  consumer.subscribe([TOPIC_NAME]);
+  consumer.consume();
+});
+
+consumer.on('data', (m) => {
+  console.log(JSON.stringify(m));
+  console.log(m.value.toString());
+});
+
+consumer.on('disconnected', (arg) => {
+  console.log('consumer disconnected. ' + JSON.stringify(arg));
+});
+
+consumer.connect();
+
